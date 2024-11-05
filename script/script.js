24b09509466ca24840baaa5e327c08cf1557c37d
hamzagbouri <hamzagbouri2004@gmail.com>
2024-11-05 17:13:19 +0100

    fninshed

--- script/script.js
+++ script/script.js
@@ -55,59 +55,91 @@ function showCard(){
                     <img src="./image/${allPets[cmp].name}.png" alt="">
 
                 </div>`
-    } else {
-        
-
-    }
+    } 
+   
     
 }
-document.getElementById("like-btn").addEventListener("click", function (){
+function endCard(){
+    cardContainer.innerHTML =` NO MORE PETS AVAILABLE `
+    const btnRefresh = document.createElement('span')
+    btnRefresh.setAttribute("id", "refresh-btn")
+    btnRefresh.innerText="Refresh"
+    document.getElementById("ignore-btn").replaceWith(btnRefresh)
+    const btnLiked = document.createElement('span')
+    btnLiked.setAttribute("id", "liked-btn")
+    btnLiked.innerText="Liked Pets"
+    document.getElementById("like-btn").replaceWith(btnLiked)
+    document.getElementById("refresh-btn").addEventListener("click", function(){
+        console.log("aaa")
+        cmp = 0;
+        likedPets = [];
+        dislikedPets = [];
+        const btnIgnore = document.createElement('span')
+        btnIgnore.setAttribute("id", "ignore-btn")
+        btnIgnore.innerText="Ignore"
+        document.getElementById("refresh-btn").replaceWith(btnIgnore)
+        const btnLike = document.createElement('span')
+        btnLike.setAttribute("id", "like-btn")
+        btnLike.innerText="Like"
+        document.getElementById("liked-btn").replaceWith(btnLike)
+
+ 
+
+        Events();
+      
+        showCard()
     
-   
-    if(cmp < l ){
-        likedPets.push(allPets[cmp])
-    console.log(likedPets)
-        if (cmp == l-1){
-            cardContainer.innerHTML =` NO MORE PETS AVAILABLE `
-                document.getElementById("like-btn").innerText ="Liked Pets"
-                document.getElementById("like-btn").setAttribute("id", "liked-btn")
-                document.getElementById("ignore-btn").innerText ="Refresh"
-                document.getElementById("ignore-btn").setAttribute("id", "refresh-btn")
-         }else {
-            cmp++;
-            showCard();
-         }
-       
-    }
     
-
-})
-document.getElementById("ignore-btn").addEventListener("click", function (){
+    })
+    document.getElementById("liked-btn").addEventListener("click", function(){
+        const div = document.createElement("div")
+        div.className = "allLiked "
+        cardContainer.innerHTML=``
+        cardContainer.appendChild(div)
+        likedPets.forEach(pet => {
+              const div2 = document.createElement("div")
+                 div2.className = "likedPets"
+                 div2.style.backgroundImage = `url(./image/${pet.name}.png)`     
+                div.appendChild(div2)
+        })
+     
     
-    if(cmp < l ){
-        dislikedPets.push(allPets[cmp])
-    console.log(dislikedPets)
-    if (cmp == l-1){
-        cardContainer.innerHTML =` NO MORE PETS AVAILABLE `
-        document.getElementById("like-btn").innerText ="Liked Pets"
-        document.getElementById("like-btn").setAttribute("id", "liked-btn")
-        document.getElementById("ignore-btn").innerText ="Refresh"
-        document.getElementById("ignore-btn").setAttribute("id", "refresh-btn")
-    }else {
-        cmp++;
-         showCard();
-        }
-    }
+    
+     })
+
+}
+Events();
+function Events(){
+            document.getElementById("like-btn").addEventListener("click", function (){
+                if(cmp < l ){
+                    likedPets.push(allPets[cmp])
+                console.log(likedPets)
+                    if (cmp == l-1){
+                        endCard();
+                    }else {
+                        cmp++;
+                        showCard();
+                    }
+                
+                }
+                
+
+            })
+
+            document.getElementById("ignore-btn").addEventListener("click", function (){
+                
+                if(cmp < l ){
+                    dislikedPets.push(allPets[cmp])
+                console.log(dislikedPets)
+                if (cmp == l-1){
+                    endCard();
+
+                }else {
+                    cmp++;
+                    showCard();
+                    }
+                }
 
 
-})
-
-document.getElementById("refresh-btn").addEventListener("click", function(){
-    cmp = 0;
-    document.getElementById("refresh-btn").innerText ="Ignore"
-    document.getElementById("refresh-btn").setAttribute("id", "ignore-btn")
-    document.getElementById("liked-btn").innerText ="Like"
-    document.getElementById("liked-btn").setAttribute("id", "like-btn")
-showCard()
-
-})
+            })
+}
