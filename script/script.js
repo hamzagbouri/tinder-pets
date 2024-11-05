a4da6989982e16b773a85c3cf4ac860200c74d2d
hamzagbouri <hamzagbouri2004@gmail.com>
2024-11-05 15:46:09 +0100

    fixing css

--- script/script.js
+++ script/script.js
@@ -22,12 +22,22 @@ const pet3 = {
     id: "2",
     name: "pet3",
     age: "2"
-
-
+}
+const pet4 = {
+    id: "3",
+    name: "pet4",
+    age: "2"
+}
+const pet5 = {
+    id: "4",
+    name: "pet5",
+    age: "2"
 }
 allPets.push(pet1)
 allPets.push(pet2)
 allPets.push(pet3)
+allPets.push(pet4)
+allPets.push(pet5)
 console.log("aaa",allPets)
 
 let l = allPets.length;
@@ -35,69 +45,69 @@ let cmp = 0;
 
 showCard()
 function showCard(){
+
     console.log(cmp<l)
     cardContainer.innerHTML=``
 
     if(cmp<l){
       
-            cardContainer.innerHTML =`   <div class="img">
-                 <img src="./image/${allPets[cmp].name}.png" alt="">
-                </div>
-                           <div class="card-text ">
-                              <span onclick="addDislikedPets()"  id="ignore-btn" class="">
-                                Ignore
-                              </span>
-                              <span onclick="addLikePets()" id="like-btn">
-                                Like
-                              </span>
-                          </div>`
+            cardContainer.innerHTML =`     <div id="card" class="card">
+                    <img src="./image/${allPets[cmp].name}.png" alt="">
+
+                </div>`
     } else {
         
 
     }
     
 }
-function addLikePets(){
+document.getElementById("like-btn").addEventListener("click", function (){
     
    
     if(cmp < l ){
         likedPets.push(allPets[cmp])
     console.log(likedPets)
         if (cmp == l-1){
-            cardContainer.innerHTML =`   <div class="nomore" style="color: white">
-                    <p> NO MORE PETS AVAILABLE </p>
-           </div>
-                      <div class="card-text ">
-                         <span onclick="addDislikedPets()"  id="ignore-btn" class="">
-                           Ignore
-                         </span>
-                         <span onclick="addLikePets()" id="like-btn">
-                           Like
-                         </span>
-                     </div>`
+            cardContainer.innerHTML =` NO MORE PETS AVAILABLE `
                 document.getElementById("like-btn").innerText ="Liked Pets"
                 document.getElementById("like-btn").setAttribute("id", "liked-btn")
+                document.getElementById("ignore-btn").innerText ="Refresh"
+                document.getElementById("ignore-btn").setAttribute("id", "refresh-btn")
          }else {
+            cmp++;
             showCard();
          }
        
     }
-    cmp++;
-
-}
-function addDislikedPets(){
-    a=true;
     
-   
+
+})
+document.getElementById("ignore-btn").addEventListener("click", function (){
+    
     if(cmp < l ){
         dislikedPets.push(allPets[cmp])
     console.log(dislikedPets)
     if (cmp == l-1){
-        cardContainer.innerHTML=`FINISHED`
-                 }else {
-                    showCard();
-                 }
+        cardContainer.innerHTML =` NO MORE PETS AVAILABLE `
+        document.getElementById("like-btn").innerText ="Liked Pets"
+        document.getElementById("like-btn").setAttribute("id", "liked-btn")
+        document.getElementById("ignore-btn").innerText ="Refresh"
+        document.getElementById("ignore-btn").setAttribute("id", "refresh-btn")
+    }else {
+        cmp++;
+         showCard();
+        }
     }
-    cmp++;
 
-}
+
+})
+
+document.getElementById("refresh-btn").addEventListener("click", function(){
+    cmp = 0;
+    document.getElementById("refresh-btn").innerText ="Ignore"
+    document.getElementById("refresh-btn").setAttribute("id", "ignore-btn")
+    document.getElementById("liked-btn").innerText ="Like"
+    document.getElementById("liked-btn").setAttribute("id", "like-btn")
+showCard()
+
+})
